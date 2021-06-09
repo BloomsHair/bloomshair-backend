34ca9e282b045ae4e3eaa56217ddfe16dfbc534b
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-06-09 23:08:15 +0100

    bloomshair-backent(dev): corrected spelling error in module import

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require( './routes/userRoutes');
 const orderRoutes = require( './routes/orderRoutes');
-const uploadRoutes = require('./routes/uploadRoutes');
+const uploadRoutes = require('./routes/upLoadRoutes');
 
 dotenv.config();
 
