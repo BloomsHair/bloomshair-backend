0efbbd7327e4fefe1e0b9fe78dec8663eae7f129
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-06-09 21:25:21 +0100

    bloomshair-backent(dev): updated package.json

--- server.js
+++ server.js
@@ -37,18 +37,6 @@ app.get('/api/config/paypal', (_, res) =>
 );
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-//   app.get('*', (_, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   );
-// } else {
-//   app.get('/api', (_, res) => {
-//     res.send('API is running....');
-//   });
-// }
-
 app.use(notFound);
 
 app.use(errorHandler);
